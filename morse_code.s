70c1889b0fafe5afb22929ac7f3f204c6074be44
Osama Aboukoura <osama.aboukoura@icloud.com>
2017-01-25 22:51:40 +0000

    Update morse_code.s

--- morse_code.s
+++ morse_code.s
@@ -9,15 +9,12 @@
 ; specify the start address
 .org 0
 
-
 main :	ldi r16,0 		; set register r16 to zero
 	out SREG,r16		; clear SREG.
 
 	ldi r16,0x01
 	out DDRB,r16
 	
-	
-
 	; THE LETTER O CONSISTS OF 3 DASHES WITH 2 SPACES IN BETWEEN
 	ldi r16,0x01		; the value 1 turns the LED on
 	out PORTB,r16
@@ -42,15 +39,11 @@ main :	ldi r16,0 		; set register r16 to zero
 	ldi r19, 60 		; keep the LED on for 600 ms
 	call routine
 
-
-
 	; THIS REPRESENTS THE SPACE BETWEEN THE LETTER O AND THE LETTER S
 	ldi r16,0x00
 	out PORTB,r16
 	ldi r19, 60		; keep the LED off for 600 ms
 	call routine
-
-	
 
 	; THE LETTER S CONSISTS OF 3 DOTS WITH 2 SPACES IN BETWEEN 
 	ldi r16,0x01		
@@ -76,15 +69,11 @@ main :	ldi r16,0 		; set register r16 to zero
 	ldi r19, 20 		; keep the LED on for 200 ms
 	call routine
 
-
-
 	; THIS REPRESENTS THE SPACE BETWEEN THE LETTER S AND THE LETTER A
 	ldi r16,0x00
 	out PORTB,r16
 	ldi r19, 60		; keep the LED off for 600 ms
 	call routine
-
-
 
 	; THE LETTER A CONSISTS OF 1 DOT THEN 1 DASH WITH 1 SPACE IN BETWEEN 
 	ldi r16,0x01
@@ -101,8 +90,6 @@ main :	ldi r16,0 		; set register r16 to zero
 	ldi r19, 60 		; keep the LED on for 600 ms
 	call routine
 	
-
-
 	; THIS REPRESENTS THE FINAL SPACE AFTER THE LETTER A 
 	ldi r16,0x00
 	out PORTB,r16
@@ -113,21 +100,20 @@ main :	ldi r16,0 		; set register r16 to zero
 mainloop: rjmp mainloop		; jump back to mainloop address
 
 routine: 
-		; delay for 10 milliseconds times parameter 
+	; delay for 10 milliseconds times parameter 
+	ldi r17, 255 
+        ldi r18, 126 
+	loop1:	nop
+		dec r17 
+		cpi r17, 0
+		brne loop1  	
 		ldi r17, 255 
-        	ldi r18, 126 
-
-		loop1:	nop
-			dec r17 
-			cpi r17, 0
-			brne loop1  	
-			ldi r17, 255 
-			dec r18 
-			cpi r18, 0
-			brne loop1 
-			ldi r18, 126 
-			dec r19 
-			cpi r19, 0
-			brne loop1
-			ret
+		dec r18 
+		cpi r18, 0
+		brne loop1 
+		ldi r18, 126 
+		dec r19 
+		cpi r19, 0
+		brne loop1
+		ret
 
