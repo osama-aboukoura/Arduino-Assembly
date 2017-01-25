09b3fa750c6275ec36cb0952d48e8f53f15f8df3
Osama Aboukoura <osama.aboukoura@icloud.com>
2017-01-25 23:23:22 +0000

    Update ping_pong.s

--- ping_pong.s
+++ ping_pong.s
@@ -15,168 +15,143 @@
 ; specify the start address
 .org 0
 
-
 main :	ldi r16,0 		; set register r16 to zero
         out SREG,r16		; clear SREG.
-
-        ldi r16,0x0F
+	
+	ldi r16,0x0F
         out DDRB,r16
 
         ldi r16,0xF0
         out DDRD,r16
 
-        ; note how we used DDRB for the lower 4 bits of a number (the first four bits)
-        ; and we then used DDRD for the upper 4 bits of a number (the last four bits)
-        ; and thats because a register cannot hadle more than 4 bits
-
-
-
-
 mainloop: 
+	ldi r19, 5          	; time delay parameter of 50 ms for the routine  
 
+	; hex numbers below 10 are written out using PORT B
+	ldi r16,0x01		; 1 (hex number)
+	out PORTB,r16
+	call routine
+	ldi r16,0x00		; the value 0 turns the LED off
+	out PORTB,r16
+	call routine
 
-ldi r19, 5          ; time delay: keep the LED on for 50 ms (it gets updated in the last line of the routine)
+	ldi r16, 0x02       	; 2 (hex number)
+	out PORTB,r16
+	call routine
+	ldi r16,0x00		; the value 0 turns the LED off
+	out PORTB,r16
+	call routine
 
+	ldi r16, 0x04       	; 4 (hex number)
+	out PORTB,r16
+	call routine
+	ldi r16,0x00		; the value 0 turns the LED off
+	out PORTB,r16
+	call routine
 
-
-; hex numbers below 10 are written out using PORT B
-
-ldi r16,0x01		; 1 (hex number)
-out PORTB,r16
-call routine
-ldi r16,0x00		; the value 0 turns the LED off
-out PORTB,r16
-call routine
-
-ldi r16, 0x02       ; 2 (hex number)
-out PORTB,r16
-call routine
-ldi r16,0x00		; the value 0 turns the LED off
-out PORTB,r16
-call routine
-
-ldi r16, 0x04       ; 4 (hex number)
-out PORTB,r16
-call routine
-ldi r16,0x00		; the value 0 turns the LED off
-out PORTB,r16
-call routine
-
-ldi r16, 0x08       ; 8 (hex number)
-out PORTB,r16
-call routine
-ldi r16,0x00		; the value 0 turns the LED off
-out PORTB,r16
-call routine
+	ldi r16, 0x08       	; 8 (hex number)
+	out PORTB,r16
+	call routine
+	ldi r16,0x00		; the value 0 turns the LED off
+	out PORTB,r16
+	call routine
 
 
 
-; hex numbers that are greater than or equal to 10 are written out using PORT D
+	; hex numbers that are greater than or equal to 10 are written out using PORT D
+	
+	ldi r16, 0x10       	; 10 (hex number)
+	out PORTD,r16
+	call routine
+	ldi r16,0x00		; the value 0 turns the LED off
+	out PORTD,r16
+	call routine
 
-ldi r16, 0x10       ; 10 (hex number)
-out PORTD,r16
-call routine
-ldi r16,0x00		; the value 0 turns the LED off
-out PORTD,r16
-call routine
+	ldi r16, 0x20       	; 20 (hex number)
+	out PORTD,r16
+	call routine
+	ldi r16,0x00		; the value 0 turns the LED off
+	out PORTD,r16
+	call routine
 
-ldi r16, 0x20       ; 20 (hex number)
-out PORTD,r16
-call routine
-ldi r16,0x00		; the value 0 turns the LED off
-out PORTD,r16
-call routine
+	ldi r16, 0x40       	; 40 (hex number)
+	out PORTD,r16
+	call routine
+	ldi r16,0x00		; the value 0 turns the LED off
+	out PORTD,r16
+	call routine
+	
+	ldi r16, 0x80       	; 80 (hex number)
+	out PORTD,r16
+	call routine
+	ldi r16,0x00		; the value 0 turns the LED off
+	out PORTD,r16
+	call routine
+	
+	; going backwards
 
-ldi r16, 0x40       ; 40 (hex number)
-out PORTD,r16
-call routine
-ldi r16,0x00		; the value 0 turns the LED off
-out PORTD,r16
-call routine
-
-ldi r16, 0x80       ; 80 (hex number)
-out PORTD,r16
-call routine
-ldi r16,0x00		; the value 0 turns the LED off
-out PORTD,r16
-call routine
-
-
-
-; going backwards
-
-ldi r16, 0x40       ; 40 (hex number)
-out PORTD,r16
-call routine
-ldi r16,0x00		; the value 0 turns the LED off
-out PORTD,r16
-call routine
-
-ldi r16, 0x20       ; 20 (hex number)
-out PORTD,r16
-call routine
-ldi r16,0x00		; the value 0 turns the LED off
-out PORTD,r16
-call routine
-
-ldi r16, 0x10       ; 10 (hex number)
-out PORTD,r16
-call routine
-ldi r16,0x00		; the value 0 turns the LED off
-out PORTD,r16
-call routine
+	ldi r16, 0x40       	; 40 (hex number)
+	out PORTD,r16
+	call routine
+	ldi r16,0x00		; the value 0 turns the LED off
+	out PORTD,r16
+	call routine
+	
+	ldi r16, 0x20       	; 20 (hex number)
+	out PORTD,r16
+	call routine
+	ldi r16,0x00		; the value 0 turns the LED off
+	out PORTD,r16
+	call routine
+	
+	ldi r16, 0x10       	; 10 (hex number)
+	out PORTD,r16
+	call routine
+	ldi r16,0x00		; the value 0 turns the LED off
+	out PORTD,r16
+	call routine
 
 
-ldi r16, 0x08       ; 8 (hex number)
-out PORTB,r16
-call routine
-ldi r16,0x00		; the value 0 turns the LED off
-out PORTB,r16
-call routine
+	ldi r16, 0x08       	; 8 (hex number)
+	out PORTB,r16
+	call routine
+	ldi r16,0x00		; the value 0 turns the LED off
+	out PORTB,r16
+	call routine
+	
+	ldi r16, 0x04       	; 4 (hex number)
+	out PORTB,r16
+	call routine
+	ldi r16,0x00		; the value 0 turns the LED off
+	out PORTB,r16
+	call routine
+	
+	ldi r16, 0x02       	; 2 (hex number)
+	out PORTB,r16
+	call routine
+	ldi r16,0x00		; the value 0 turns the LED off
+	out PORTB,r16
+	call routine
 
-ldi r16, 0x04       ; 4 (hex number)
-out PORTB,r16
-call routine
-ldi r16,0x00		; the value 0 turns the LED off
-out PORTB,r16
-call routine
-
-ldi r16, 0x02       ; 2 (hex number)
-out PORTB,r16
-call routine
-ldi r16,0x00		; the value 0 turns the LED off
-out PORTB,r16
-call routine
-
-
-
-
-
-
-
-rjmp mainloop		; jump back to mainloop address
-; end of main code is here
-
-
-; this is a method (or a function) that gets called within the program using the command: call
+	rjmp mainloop		; jump back to mainloop address
 
 routine:
-		; delay for 10 milliseconds times parameter
-		ldi r17, 255 
-        	ldi r18, 126
+	; delay for 10 milliseconds times parameter
+	ldi r17, 255 
+        ldi r18, 126
 
-		loop1:	nop
-			dec r17 
-			cpi r17, 0
-			brne loop1  	
-			ldi r17, 255 
-			dec r18 
-			cpi r18, 0
-			brne loop1 
-			ldi r18, 126 
-			dec r19 
-			cpi r19, 0
-			brne loop1
-            ldi r19, 5
-			ret
+	loop1:	nop
+		dec r17 
+		cpi r17, 0
+		brne loop1  	
+		ldi r17, 255 
+		dec r18 
+		cpi r18, 0
+		brne loop1 
+		ldi r18, 126 
+		dec r19 
+		cpi r19, 0
+		brne loop1
+            	ldi r19, 5
+		ret
 			
